2db5ba4c32c0698f1f4f75044fffecc69b6d831d
Pavel Halukha <pavelgaluha@gmail.com>
2026-03-10 12:48:47 +0300

    add home bloc

---
+++ frontend/lib/feature/home/presentation/bloc/home_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:cookify/feature/home/domain/usecase/get_recipe_list.dart';
+import 'package:cookify/feature/home/presentation/bloc/home_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeCubit extends Cubit<HomeState> {
+  HomeCubit({required GetRecipeList getRecipeList})
+    : 
+      _getRecipeList = getRecipeList,
+      super(HomeInitial());
+
+  final GetRecipeList _getRecipeList;
+
+  Future<void> getRecipeList() async {
+    emit(HomeLoading());
+
+    final recipesOrFailure = await _getRecipeList();
+
+    if (!isClosed) {
+      emit(
+        recipesOrFailure.fold(
+          (failure) => HomeError(failure: failure),
+          (recipes) => HomeLoaded(recipes: recipes),
+        ),
+      );
+    }
+  }
+}
