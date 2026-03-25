70e68e9ff07c7cc8e4210b68e437bcb0500e8f30
Maksim Kuchko <kuchkomaxim2527@gmail.com>
2026-03-25 14:37:38 +0300

    init migrations, init new bd

---
+++ backend/CookifyAPI/CookifyAPI/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using CookifyAPI.Data;
+using CookifyAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly AppDbContext _db;
+
+    public UsersController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<User>> Get()
+    {
+        return await _db.Users.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(User user)
+    {
+        _db.Users.Add(user);
+        await _db.SaveChangesAsync();
+        return Ok(user);
+    }
+}
---
+++ backend/CookifyAPI/CookifyAPI/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace CookifyAPI.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+}
