919eb089c19a6ab1c2d05486a7e78b8b0c3c0875
Pavel Halukha <pavelgaluha@gmail.com>
2026-03-28 02:18:46 +0300

    add recipe remote data source

---
+++ frontend/lib/config/util/safecall/safe_data_source_call.dart
@@ -0,0 +1,16 @@
+import 'package:cookify/config/error/exception.dart';
+import 'package:cookify/config/error/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+Future<Either<Failure, T>> safeRemoteDataSourceCall<T>(
+  Future<T> Function() call,
+) async {
+  try {
+    final result = await call();
+
+    return Right(result);
+  } on DioException {
+    return Left(ServerFailure());
+  }
+}
