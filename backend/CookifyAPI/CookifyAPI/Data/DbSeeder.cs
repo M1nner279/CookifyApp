00804dc61381566b6ad51c0a3e052ea17a8d7f48
Maksim Kuchko <kuchkomaxim2527@gmail.com>
2026-03-27 23:44:48 +0300

    add ingredients in seed

--- backend/CookifyAPI/CookifyAPI/Data/DbSeeder.cs
+++ backend/CookifyAPI/CookifyAPI/Data/DbSeeder.cs
@@ -26,8 +26,27 @@ public static class DbSeeder
             new Tag { Name = "Выпечка" }
         };
 
-        context.Tags.AddRange(tags);
+        var ingredients = new List<Ingredient>
+        {
+            new Ingredient { Name = "Курица бройлерная" },
+            new Ingredient { Name = "Рис" },
+            new Ingredient { Name = "Чеснок" },
+            new Ingredient { Name = "Масло сливочное" },
+            new Ingredient { Name = "Лук репчатый" },
+            new Ingredient { Name = "Морковь" },
+            new Ingredient { Name = "Томатная паста" },
+            new Ingredient { Name = "Зелень" },
+            new Ingredient { Name = "Соль" },
+            new Ingredient { Name = "Вода" },
+            new Ingredient { Name = "Картофель" },
+            new Ingredient { Name = "Сметана" },
+            new Ingredient { Name = "Мука" },
+        };
+        
         context.Users.Add(user);
+        context.Tags.AddRange(tags);
+        context.Ingredients.AddRange(ingredients);
+
         await context.SaveChangesAsync(); // Сохраняем пользователя, чтобы EF сгенерировал ID
         
         var recipe = new Recipe
@@ -44,56 +63,16 @@ public static class DbSeeder
         await context.SaveChangesAsync();
         
         context.RecipeImages.AddRange(
-            new RecipeImage
-            {
-                RecipeId = recipe.Id,
-                Url = "https://www.russianfood.com/dycontent/images_upl/4/big_3618.jpg",
-                Order = 0 // превью
-            },
-            new RecipeImage
-            {
-                RecipeId = recipe.Id,
-                Url = "https://www.russianfood.com/dycontent/images_upl/4/big_3619.jpg",
-                Order = 1
-            },
-            new RecipeImage
-            {
-                RecipeId = recipe.Id,
-                Url = "https://www.russianfood.com/dycontent/images_upl/4/big_3620.jpg",
-                Order = 2
-            },
-            new RecipeImage
-            {
-                RecipeId = recipe.Id,
-                Url = "https://www.russianfood.com/dycontent/images_upl/4/big_3621.jpg",
-                Order = 3
-            }
+            new RecipeImage { RecipeId = recipe.Id, Url = "https://www.russianfood.com/dycontent/images_upl/4/big_3618.jpg", Order = 0 },
+            new RecipeImage { RecipeId = recipe.Id, Url = "https://www.russianfood.com/dycontent/images_upl/4/big_3619.jpg", Order = 1 },
+            new RecipeImage { RecipeId = recipe.Id, Url = "https://www.russianfood.com/dycontent/images_upl/4/big_3620.jpg", Order = 2 },
+            new RecipeImage { RecipeId = recipe.Id, Url = "https://www.russianfood.com/dycontent/images_upl/4/big_3621.jpg", Order = 3 }
         );
 
         context.RecipeSteps.AddRange(
-            new RecipeStep
-            {
-                RecipeId = recipe.Id,
-                StepNumber = 1,
-                Title = "1 шак",
-                Description = "Картофель натереть на крупной тёрке, отжать, добавить муку, сметану, соль, перемешать.",
-                ImageUrl = "https://www.russianfood.com/dycontent/images_upl/4/big_3619.jpg"
-            },
-            new RecipeStep
-            {
-                RecipeId = recipe.Id,
-                StepNumber = 2,
-                Title = "title нужен?",
-                Description = "Выкладывать столовой ложкой на очень хорошо разогретую сковороду в растопленное масло.",
-                ImageUrl = "https://www.russianfood.com/dycontent/images_upl/4/big_3620.jpg"
-            },
-            new RecipeStep
-            {
-                RecipeId = recipe.Id,
-                StepNumber = 3,
-                Description = "Обжаривать драники с обеих сторон до золотистой корочки. Подавать горячими со сметаной.",
-                ImageUrl = "https://www.russianfood.com/dycontent/images_upl/4/big_3621.jpg"
-            }
+            new RecipeStep { RecipeId = recipe.Id, StepNumber = 1, Title = "1 шак", Description = "Картофель натереть на крупной тёрке, отжать, добавить муку, сметану, соль, перемешать.", ImageUrl = "https://www.russianfood.com/dycontent/images_upl/4/big_3619.jpg" },
+            new RecipeStep { RecipeId = recipe.Id, StepNumber = 2, Title = "title нужен?", Description = "Выкладывать столовой ложкой на очень хорошо разогретую сковороду в растопленное масло.", ImageUrl = "https://www.russianfood.com/dycontent/images_upl/4/big_3620.jpg" },
+            new RecipeStep { RecipeId = recipe.Id, StepNumber = 3, Description = "Обжаривать драники с обеих сторон до золотистой корочки. Подавать горячими со сметаной.", ImageUrl = "https://www.russianfood.com/dycontent/images_upl/4/big_3621.jpg" }
         );
 
         await context.SaveChangesAsync();
@@ -132,23 +111,26 @@ public static class DbSeeder
 
         var recipeTags = new List<RecipeTag>
         {
-            new RecipeTag
-            {
-                RecipeId = recipe.Id,
-                TagId = tags.First(t => t.Name == "Быстро").Id
-            },
-            new RecipeTag
-            {
-                RecipeId = recipe.Id,
-                TagId = tags.First(t => t.Name == "Суп").Id
-            },
-            new RecipeTag
-            {
-                RecipeId = recipe.Id,
-                TagId = tags.First(t => t.Name == "Завтрак").Id
-            }
+            new RecipeTag { RecipeId = recipe.Id, TagId = tags.First(t => t.Name == "Быстро").Id },
+            new RecipeTag { RecipeId = recipe.Id, TagId = tags.First(t => t.Name == "Суп").Id },
+            new RecipeTag { RecipeId = recipe.Id, TagId = tags.First(t => t.Name == "Завтрак").Id }
         };
         context.AddRange(recipeTags);
+        var recipeIngredients = new List<RecipeIngredient>
+        {
+            new RecipeIngredient { RecipeId = recipe.Id, IngredientId = ingredients.First(t => t.Name == "Курица бройлерная").Id, Amount = 1, Unit = "шт."},
+            new RecipeIngredient { RecipeId = recipe.Id, IngredientId = ingredients.First(t => t.Name == "Рис").Id, Amount = 0.5f, Unit = "стакана"},
+            new RecipeIngredient { RecipeId = recipe.Id, IngredientId = ingredients.First(t => t.Name == "Чеснок").Id, Amount = 1, Unit = "головка"},
+            new RecipeIngredient { RecipeId = recipe.Id, IngredientId = ingredients.First(t => t.Name == "Масло сливочное").Id, Amount = 50, Unit = "г"},
+            new RecipeIngredient { RecipeId = recipe.Id, IngredientId = ingredients.First(t => t.Name == "Лук репчатый").Id, Amount = 1, Unit = "шт."},
+            new RecipeIngredient { RecipeId = recipe.Id, IngredientId = ingredients.First(t => t.Name == "Морковь").Id, Amount = 1, Unit = "шт."},
+            new RecipeIngredient { RecipeId = recipe.Id, IngredientId = ingredients.First(t => t.Name == "Томатная паста").Id, Amount = 2, Unit = "ст. ложки"},
+            new RecipeIngredient { RecipeId = recipe.Id, IngredientId = ingredients.First(t => t.Name == "Зелень").Id, Amount = 55, Unit = "г"}, // Среднее значение от 50-60
+            new RecipeIngredient { RecipeId = recipe.Id, IngredientId = ingredients.First(t => t.Name == "Соль").Id, Amount = 1, Unit = "ст. ложка"},
+            new RecipeIngredient { RecipeId = recipe.Id, IngredientId = ingredients.First(t => t.Name == "Вода").Id, Amount = 2.5f, Unit = "л"}
+
+        };
+        context.AddRange(recipeIngredients);
         
         await context.SaveChangesAsync();
 
--- backend/CookifyAPI/CookifyAPI/Migrations/20260327203846_miniFix.Designer.cs
+++ backend/CookifyAPI/CookifyAPI/Migrations/20260327203846_miniFix.Designer.cs
@@ -4,6 +4,7 @@ using CookifyAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CookifyAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260327203846_miniFix")]
+    partial class miniFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
