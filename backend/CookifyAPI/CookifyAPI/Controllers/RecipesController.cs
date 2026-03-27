41a6b6bff41f5165b723a9dd0dab3aa09a2e52e4
Maksim Kuchko <kuchkomaxim2527@gmail.com>
2026-03-27 04:30:45 +0300

    add API for list and single recipe

---
+++ backend/CookifyAPI/CookifyAPI/Controllers/RecipesController.cs
@@ -0,0 +1,45 @@
+﻿using CookifyAPI.Data;
+using CookifyAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookifyAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RecipesController : ControllerBase
+{
+    private readonly AppDbContext _db;
+
+    public RecipesController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    // GET: api/recipes
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Recipe>>> GetRecipes()
+    {
+        var recipes = await _db.Recipes.ToListAsync();
+        return Ok(recipes);
+    }
+    
+    // GET: api/recipes/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Recipe>> GetRecipe(int id)
+    {
+        var recipe = await _db.Recipes.FindAsync(id);
+        if (recipe == null)
+            return NotFound();
+
+        return Ok(recipe);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Recipe recipe)
+    {
+        _db.Recipes.Add(recipe);
+        await _db.SaveChangesAsync();
+        return Ok(recipe);
+    }
+}
