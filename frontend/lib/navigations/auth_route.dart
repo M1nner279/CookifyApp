50050ac6085be0b34d61cc5410925dffe6fd479c
Pavel Halukha <pavelgaluha@gmail.com>
2026-04-24 01:05:56 +0300

    fix transitions

--- frontend/lib/navigations/auth_route.dart
+++ frontend/lib/navigations/auth_route.dart
@@ -2,22 +2,27 @@ import 'package:cookify/features/auth/auth_common/presentation/pages/auth_page.d
 import 'package:cookify/features/auth/change_password/presentation/pages/change_password_page.dart';
 import 'package:cookify/features/auth/otp/presentation/pages/otp_page.dart';
 import 'package:cookify/features/auth/otp/presentation/pages/otp_page_args.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final authRoute = [
-  GoRoute(path: '/auth', builder: (context, state) => const AuthPage()),
+  GoRoute(
+    path: '/auth',
+    pageBuilder: (context, state) => MaterialPage(child: const AuthPage()),
+  ),
 
   GoRoute(
     path: '/otp',
-    builder: (context, state) {
+    pageBuilder: (context, state) {
       final args = state.extra as OtpPageArgs;
 
-      return OtpPage(args: args);
+      return MaterialPage(child: OtpPage(args: args));
     },
   ),
 
   GoRoute(
     path: '/change-password',
-    builder: (context, state) => const ChangePasswordPage(),
+    pageBuilder: (context, state) =>
+        MaterialPage(child: const ChangePasswordPage()),
   ),
 ];
